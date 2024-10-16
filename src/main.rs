e5db2a90e1248cc1dbf749d7ab73975a39fe6389
Francis Gulotta <wizard@roborooter.com>
2024-10-16 14:18:38 -0400

    good enough

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use reqwest::{blocking::Client, header};
 use serde;
 use serde::Deserialize;
 use serde_json;
+use std::fmt;
 use std::process::exit;
-use std::time::Duration;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug, Clone)]
-#[command(version, about = "", long_about = None)]
+#[command(version, about = " I needed a cli for render.com and I wanted to play with rust so it's a rust cli for triggering deploys on render.com", long_about = None)]
 struct Config {
     /// name of your service
     name: String,
@@ -20,6 +22,15 @@ struct Config {
 
     #[arg(short, long, env("RENDER_API_KEY"))]
     api_key: String,
+
+    /// wait for deploy timeout in seconds, doesn't cancel the deploy just exits
+    #[arg(short, long, default_value="600", value_parser = parse_duration)]
+    timeout: Duration,
+}
+
+fn parse_duration(arg: &str) -> Result<Duration, std::num::ParseIntError> {
+    let seconds = arg.parse::<u64>()?;
+    Ok(Duration::from_secs(seconds))
 }
 
 fn deserialize_yes_no<'de, D>(deserializer: D) -> Result<bool, D::Error>
@@ -45,6 +56,10 @@ struct Service {
     #[serde(rename = "autoDeploy", deserialize_with = "deserialize_yes_no")]
     auto_deploy: bool,
     repo: String,
+    #[serde(rename = "updatedAt")]
+    updated_at: String,
+    #[serde(rename = "createdAt")]
+    created_at: String,
 }
 
 #[derive(PartialEq, Deserialize, Debug, Clone)]
@@ -124,6 +139,24 @@ enum DeployStatus {
     PreDeployFailed,
 }
 
+impl fmt::Display for DeployStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let status_str = match self {
+            DeployStatus::Created => "Created",
+            DeployStatus::BuildInProgress => "Build In Progress",
+            DeployStatus::UpdateInProgress => "Update In Progress",
+            DeployStatus::Live => "Live",
+            DeployStatus::Deactivated => "Deactivated",
+            DeployStatus::BuildFailed => "Build Failed",
+            DeployStatus::UpdateFailed => "Update Failed",
+            DeployStatus::Canceled => "Canceled",
+            DeployStatus::PreDeployInProgress => "Pre-Deploy In Progress",
+            DeployStatus::PreDeployFailed => "Pre-Deploy Failed",
+        };
+        write!(f, "{}", status_str)
+    }
+}
+
 #[derive(PartialEq, Deserialize, Debug, Clone)]
 struct Deploy {
     id: String,
@@ -194,6 +227,81 @@ mod tests {
     }
 }
 
+fn deploy_url(service: &Service, deploy: &Deploy) -> String {
+    format!(
+        "https://dashboard.render.com/web/{service_id}/deploys/{deploy_id}",
+        deploy_id = deploy.id,
+        service_id = service.id
+    )
+}
+
+#[derive(PartialEq, Deserialize, Debug, Clone)]
+struct ListDeploysResponse {
+    cursor: String,
+    deploy: Deploy,
+}
+
+fn latest_deploy(client: &Client, service: &Service) -> Option<Deploy> {
+    let response = client
+        .get(format!(
+            "https://api.render.com/v1/services/{}/deploys",
+            service.id
+        ))
+        .query(&[("limit", "1".to_string())])
+        .send()
+        .expect("Could not build request latest_deploy");
+    if !response.status().is_success() {
+        println!(
+            "Request Error: {:?} {:?}",
+            response.status(),
+            response.text().unwrap_or("Unknown Error".into())
+        );
+        exit(1);
+    }
+    let body = response.text().expect("unable to read response body");
+
+    let deploys: Vec<ListDeploysResponse> = match serde_json::from_str(&body) {
+        Ok(services) => services,
+        Err(e) => {
+            println!("Unable to parse json {:?}", e);
+            println!("{}", body);
+            exit(1);
+        }
+    };
+    deploys.into_iter().next().map(|resp| resp.deploy)
+}
+
+fn get_deploy(client: &Client, service: &Service, deploy_id: &String) -> Option<Deploy> {
+    let response = client
+        .get(format!(
+            "https://api.render.com/v1/services/{service_id}/deploys/{deploy_id}",
+            service_id = service.id,
+            deploy_id = deploy_id
+        ))
+        .query(&[("limit", "1".to_string())])
+        .send()
+        .expect("Could not build request latest_deploy");
+    if !response.status().is_success() {
+        println!(
+            "Request Error: {:?} {:?}",
+            response.status(),
+            response.text().unwrap_or("Unknown Error".into())
+        );
+        exit(1);
+    }
+    let body = response.text().expect("unable to read response body");
+
+    let deploy: Deploy = match serde_json::from_str(&body) {
+        Ok(services) => services,
+        Err(e) => {
+            println!("Unable to parse json {:?}", e);
+            println!("{}", body);
+            exit(1);
+        }
+    };
+    Some(deploy)
+}
+
 fn main() {
     let config = Config::parse();
     let client = http_client(&config);
@@ -205,26 +313,94 @@ fn main() {
         }
         Some(service) => service,
     };
+    println!(
+        "Found {name} {dashboard}",
+        name = service.name,
+        dashboard = service.dashboard_url
+    );
     if service.auto_deploy {
-        println!("Warning: AutoDeploy is true for {}", service.name)
+        println!("Warning: AutoDeploy is true");
     }
 
-    println!("{:?}", service);
+    if config.commit.is_some() {
+        println!(
+            "Deploying {repo} #{commit}",
+            repo = service.repo,
+            commit = config.commit.clone().unwrap()
+        );
+    } else {
+        println!(
+            "Deploying {repo} #{branch}",
+            repo = service.repo,
+            branch = service.branch
+        );
+    }
+    print!("\n");
+
+    let previous_deploy = latest_deploy(&client, &service);
+    if previous_deploy.is_some() {
+        let deploy = previous_deploy.unwrap();
+        println!(
+            "Previous Deploy {commit} - {message}",
+            commit = deploy.commit.id,
+            message = deploy.commit.message
+        );
+        println!(
+            "Status: {status} on {finished_at}",
+            status = deploy.status,
+            finished_at = deploy.finished_at.unwrap_or("".into())
+        );
+        print!("\n");
+    }
 
     // trigger deploy
     let deploy = trigger_deploy(&client, &service, &config).unwrap();
-    println!("{:?}", deploy);
+    println!(
+        "Created Deploy {commit} - {message}",
+        commit = deploy.commit.id,
+        message = deploy.commit.message
+    );
+    println!("{}", deploy_url(&service, &deploy));
+    println!("Status: {status}", status = deploy.status);
 
     // if error error
 
     if config.wait {
-        // poll for a good status or a bad status
-        // if error error
+        let start = Instant::now();
+        loop {
+            if start.elapsed() > config.timeout {
+                println!("Deploy timed out");
+                exit(1);
+            }
+            sleep(Duration::from_secs(5));
+            let deploy = get_deploy(&client, &service, &deploy.id).unwrap();
+            println!("Status: {status}", status = deploy.status);
+            match deploy.status {
+                DeployStatus::Live => {
+                    println!(
+                        "Deploy is live on {} in {} seconds",
+                        deploy.finished_at.unwrap_or("unknown".into()),
+                        start.elapsed().as_secs()
+                    );
+                    break;
+                }
+                DeployStatus::BuildInProgress
+                | DeployStatus::UpdateInProgress
+                | DeployStatus::PreDeployInProgress
+                | DeployStatus::Created => (),
+                DeployStatus::BuildFailed
+                | DeployStatus::UpdateFailed
+                | DeployStatus::Canceled
+                | DeployStatus::Deactivated
+                | DeployStatus::PreDeployFailed => {
+                    println!(
+                        "Deploy has Stopped {}",
+                        deploy.finished_at.unwrap_or("unknown".into())
+                    );
+                    break;
+                }
+            }
+        }
     }
-    // fin!
-
-    println!(
-        "deploying {:?} with commit {:?} and wait:{:?}",
-        config.name, config.commit, config.wait
-    );
+    exit(0);
 }
